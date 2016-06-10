1f8937e2141468eb4d886377d9eea86664c1738f
YV83 <YV83@192.168.56.1>
2016-06-10 22:18:21 +0530

    master: adding test case Book

---
+++ src/test/java/com/praveen/BookResourceTest.java
@@ -0,0 +1,44 @@
+package com.praveen;
+
+import static org.junit.Assert.*;
+
+import java.util.Collection;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.GenericType;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.TestProperties;
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class BookResourceTest extends JerseyTest{
+
+	protected Application configure(){
+		enable(TestProperties.LOG_TRAFFIC);
+		enable(TestProperties.DUMP_ENTITY);
+		return new ResourceConfig().packages("com.praveen");
+		
+	}
+
+	@Test
+	public void testGetBooks() {
+		Collection<Book> response = target("books").request().get(new GenericType<Collection<Book>>(){});
+		assertEquals(2,response.size());
+	}
+
+	@Test
+	public void testGetBook() {
+		Book response = target("books").path("1").request().get(Book.class);
+		assertNotNull(response);
+	}
+	
+	@Test
+	public void testDAO(){
+		Book response1 = target("books").path("1").request().get(Book.class);
+		Book response2 = target("books").path("1").request().get(Book.class);
+		assertEquals(response1.getPublished().getTime(), response2.getPublished().getTime());
+	}
+
+}
