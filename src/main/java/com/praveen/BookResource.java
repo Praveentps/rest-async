d79dbfc34cd422da76b827a330688df8c1c67e1c
YV83 <YV83@192.168.56.1>
2016-06-11 09:27:01 +0530

    master:fixing bug in BookDao

--- src/main/java/com/praveen/BookResource.java
+++ src/main/java/com/praveen/BookResource.java
@@ -4,7 +4,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+
 import java.util.Collection;
 
 /**
@@ -12,7 +14,8 @@ import java.util.Collection;
  */
 @Path("/books")
 public class BookResource {
-BookDao dao = new BookDao();
+//BookDao dao = new BookDao();
+	@Context BookDao dao ;
 //http://localhost:8080/myapp/books
     @GET
     @Produces(MediaType.APPLICATION_JSON)
--- src/test/java/com/praveen/BookResourceTest.java
+++ src/test/java/com/praveen/BookResourceTest.java
@@ -7,6 +7,7 @@ import java.util.Collection;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.GenericType;
 
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 import org.glassfish.jersey.test.TestProperties;
@@ -18,7 +19,15 @@ public class BookResourceTest extends JerseyTest{
 	protected Application configure(){
 		enable(TestProperties.LOG_TRAFFIC);
 		enable(TestProperties.DUMP_ENTITY);
-		return new ResourceConfig().packages("com.praveen");
+		final BookDao dao = new BookDao();
+		return new ResourceConfig()
+		.packages("com.praveen")
+		.register(new AbstractBinder() {			
+			protected void configure() {
+				bind(dao).to(BookDao.class);
+				
+			}
+		});
 		
 	}
 
